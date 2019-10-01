9d42602ba239ae1a8f860840fc577eaeb64bb955
Nishit Singh <ns2023@gmail.com>
2019-10-01 12:36:21 +0530

    Runnable

--- app/src/main/java/com/example/undarkened/MainActivity.java
+++ app/src/main/java/com/example/undarkened/MainActivity.java
@@ -48,9 +48,9 @@ public class MainActivity extends AppCompatActivity {
                 R.id.nav_tools, R.id.nav_share, R.id.nav_send)
                 .setDrawerLayout(drawer)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        /*NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        NavigationUI.setupWithNavController(navigationView, navController);*/
     }
 
     @Override
