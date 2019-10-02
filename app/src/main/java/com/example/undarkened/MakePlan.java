a14f91078534d8749ee099ddee42442c69b0116a
VirenderRana <virender.rana0009@gmail.com>
2019-10-02 15:32:07 +0530

    design and page connectivity improvements.

--- app/src/main/java/com/example/undarkened/MakePlan.java
+++ app/src/main/java/com/example/undarkened/MakePlan.java
@@ -27,7 +27,7 @@ public class MakePlan extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.make_plan_activity);
 
 
     }
