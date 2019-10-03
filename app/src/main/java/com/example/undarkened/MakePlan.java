3422f20a4cdf3617f5f611a7206bef26a98c239d
Rishabh-1999 <rishabhanand33@gmail.com>
2019-10-03 14:21:44 +0530

    MakePlan Updated

--- app/src/main/java/com/example/undarkened/MakePlan.java
+++ app/src/main/java/com/example/undarkened/MakePlan.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -17,7 +18,10 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
@@ -27,12 +31,19 @@ import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashMap;
+
 public class MakePlan extends AppCompatActivity {
 
     private DatabaseReference plandata;
+    DatabaseReference reference;
+    FirebaseAuth mAuth;
+    FirebaseUser fuser;
     private Button btn;
     private EditText date;
     private CalendarView calendarView;
+    private FusedLocationProviderClient fusedLocationClient;
+    Location reallocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +54,32 @@ public class MakePlan extends AppCompatActivity {
         btn = (Button) findViewById(R.id.sendtodatabase);
         date = (EditText) findViewById(R.id.time);
         calendarView = (CalendarView) findViewById(R.id.calendarView);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseUser user = mAuth.getCurrentUser();
+                String userid = user.getUid();
+                DatabaseReference reference1 = FirebaseDatabase.getInstance().getReference("Users").child(userid).child("Plan");
 
+                HashMap<String, Object> map = new HashMap<>();
+
+                map.put("location", reallocation.toString());
+
+                reference1.updateChildren(map);
+            }
+        });
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Logic to handle location object
+                            reallocation=location;
+                        }
+                    }
+                });
     }
 
 }
